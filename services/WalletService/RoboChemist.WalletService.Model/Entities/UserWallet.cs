a3321d067458f1749faec552ab6a6e52bd46c812
Thanh bình <binhnt1509204@gmail.com>
2025-10-20 00:59:33 +0700

    chore(wallet-service): Dependency update and connect database

---
+++ services/WalletService/RoboChemist.WalletService.Model/Entities/UserWallet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoboChemist.WalletService.Model.Entities;
+
+public partial class UserWallet
+{
+    public Guid WalletId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public decimal Balance { get; set; }
+
+    public DateTime? UpdateAt { get; set; }
+
+    public virtual ICollection<WalletTransaction> WalletTransactions { get; set; } = new List<WalletTransaction>();
+}
---
+++ services/WalletService/RoboChemist.WalletService.Model/Entities/WalletTransaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoboChemist.WalletService.Model.Entities;
+
+public partial class WalletTransaction
+{
+    public Guid TransactionId { get; set; }
+
+    public Guid WalletId { get; set; }
+
+    public string TransactionType { get; set; } = null!;
+
+    public decimal Amount { get; set; }
+
+    public string Method { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public Guid? ReferenceId { get; set; }
+
+    public DateTime? CreateAt { get; set; }
+
+    public DateTime? UpdateAt { get; set; }
+
+    public virtual UserWallet Wallet { get; set; } = null!;
+}
