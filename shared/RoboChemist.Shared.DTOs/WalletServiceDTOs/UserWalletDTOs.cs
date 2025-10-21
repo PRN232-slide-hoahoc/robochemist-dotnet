46a98410cd2c96471a7b4905513828f88bc6b2c7
Thanh bình <binhnt1509204@gmail.com>
2025-10-21 14:42:23 +0700

    chore(wallet-service): init DTOs for wallet_service

---
+++ shared/RoboChemist.Shared.DTOs/WalletServiceDTOs/UserWalletDTOs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboChemist.Shared.DTOs.WalletServiceDTOs
+{
+    public class UserWalletDTOs
+    {
+        public class UserWalletDTO
+        {
+            public Guid WalletId { get; set; }
+            public Guid UserId { get; set; }
+            public decimal Balance { get; set; }
+            public DateTime? UpdateAt { get; set; }
+        }
+    }
+}
---
+++ shared/RoboChemist.Shared.DTOs/WalletServiceDTOs/WalletTransactionDTOs.cs
@@ -0,0 +1,30 @@
+﻿namespace RoboChemist.Shared.DTOs.WalletServiceDTOs
+{
+    public class WalletTransactionDTOs
+    {
+        public class WalletTransactionDto
+        {
+            public Guid TransactionId { get; set; }
+            public Guid? UserId { get; set; }
+            public Guid WalletId { get; set; }
+            public string TransactionType { get; set; } = string.Empty;
+            public decimal Amount { get; set; }
+            public string Method { get; set; } = string.Empty;
+            public string Status { get; set; } = string.Empty;
+            public Guid? ReferenceId { get; set; }
+            public DateTime? CreateAt { get; set; }
+            public DateTime? UpdateAt { get; set; }
+        }
+        
+        public class CreateWalletTransactionDto
+        {
+            public Guid WalletId { get; set; }
+            public string TransactionType { get; set; } = string.Empty;
+            public decimal Amount { get; set; }
+            public string Method { get; set; } = string.Empty;
+            public string Status { get; set; } = string.Empty;
+            public Guid? ReferenceId { get; set; }
+            public DateTime? CreateAt { get; set; } = DateTime.UtcNow;
+        }
+    }
+}
