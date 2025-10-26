a5d7597e0e20f4fa30992d5f37193cbc7331158e
BinhNTSE181790 <BinhNTSE181790@fpt.edu.vn>
2025-10-26 10:42:04 +0700

    chore: Create VNPay DTO

---
+++ shared/RoboChemist.Shared.DTOs/WalletServiceDTOs/VNPayDTOs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboChemist.Shared.DTOs.WalletServiceDTOs
+{
+    public class VNPayDTOs
+    {
+        public class DepositRequestDTO
+        {
+            public string token { get; set; }
+            public decimal amount { get; set; }
+        }
+
+        public class VNPayConfig
+        {
+            public string TmnCode { get; set; }
+            public string HashSecret { get; set; }
+            public string VnpayUrl { get; set; }
+            public string CallbackUrl { get; set; }
+        }
+    }
+}
